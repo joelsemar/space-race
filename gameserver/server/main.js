0e9359f464f61eee815ea30ef78853f304b0b7d9
Joel Semar <semarjt@gmail.com>
2017-04-21 10:14:37 -0500

    command line argument token/node name

--- gameserver/server/main.js
+++ gameserver/server/main.js
@@ -3,13 +3,6 @@ var GameServer = require("./gameserver.js"),
     ApiClient = require("./api.js"),
     CONFIG = require("./config.js");
 
-var apiClient = new ApiClient(CONFIG.apiHost);
-var gameServer = new GameServer({
-    apiClient: apiClient,
-    host: CONFIG.host,
-    port: CONFIG.gameServerPort
-});
-gameServer.run();
 RUNNING_ON_CLIENT = false;
 
 updateClients = function (data) {
@@ -23,8 +16,24 @@ releaseGameNode = function () {
     gameServer.nodeReset()
 }
 
+let nodeTag = "n1";
+if (process.argv.length > 2){
+    nodeTag = process.argv[2];
+
+}
+
+var apiClient = new ApiClient(CONFIG.apiHost, nodeTag + "-game_node_token");
+var gameServer = new GameServer({
+    apiClient: apiClient,
+    host: CONFIG.host,
+    port: CONFIG.gameServerPort
+});
+
+gameServer.run();
+
+
 if (CONFIG.lobby) {
-    var apiClient = new ApiClient(CONFIG.apiHost, "./chat_node_token");
+    var apiClient = new ApiClient(CONFIG.apiHost, nodeTag + "-chat_node_token");
     new LobbyServer({
         apiClient: apiClient,
         host: CONFIG.host,
