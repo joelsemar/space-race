49332631d2ecfdc730884c2cb7b513b4442f1eca
Joel Semar <semarjt@gmail.com>
2017-04-26 18:08:57 -0500

    more node host defs

--- apiserver/apiserver/dev_settings.py
+++ apiserver/apiserver/dev_settings.py
@@ -1,3 +1,3 @@
-NODE_HOSTS = ["dev.planetgrab.com"]
+NODE_HOSTS = ["dev.planetgrab.com", "45.33.40.59"]
 LOBBY_LOCATION = 'http://dev.planetgrab.com/'
 GAME_LOCATION = 'http://dev.planetgrab.com/play'
